92f70a88ee7901662d4eb6721b0e8eb530af1394
Udaykalse <udaysinhk.2000@gmail.com>
2025-12-26 10:32:10 +0530

    Python Project :- 2. (calendar)

---
+++ Project/Python/Calendar/DemoCalendar.py
@@ -0,0 +1,16 @@
+import calendar
+
+year=int(input('Entre Year :- '))
+month=int(input('Entre Month :- '))
+
+cal=calendar.monthcalendar(year,month)
+
+print('Mo Tu We Th Fr Sa Su')
+for week in cal:
+    for day in week:
+        if day == 0:
+            print(" ",end=" ")
+        else:
+            print(f"{day:2}",end=" ")
+    print()
+
