edcbf0216a17c3aeb61d1636f8473bf6c8e715ee
Udaykalse <udaysinhk.2000@gmail.com>
2026-01-24 23:03:16 +0530

    Day 31 Python

---
+++ Python/P30/App10.py
@@ -0,0 +1,8 @@
+n = 1234
+total = 0
+
+while n > 0:
+    total += n % 10
+    n //= 10
+
+print(total)
---
+++ Python/P30/App11.py
@@ -0,0 +1,8 @@
+n = 7
+is_prime = True
+
+for i in range(2, n):
+    if n % i == 0:
+        is_prime = False
+
+print(is_prime)
