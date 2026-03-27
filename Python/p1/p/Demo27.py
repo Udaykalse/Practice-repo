847a7ff7000722ff04371d02569fb6f32f9b6915
Udaykalse <udaysinhk.2000@gmail.com>
2026-03-27 21:11:24 +0530

    python p6 #5

--- Python/p1/p/Demo27.py
+++ Python/p1/p/Demo27.py
@@ -7,3 +7,13 @@ for ch in s:
 
 
 print(rev)
+
+
+
+# Hi Uday,
+
+# I hope this message finds you well. I came across your profile and was impressed by your experience as a Fullstack Developer at FirstStore Ricoz, especially your work on scalable web applications.
+
+# As an AWS Certified Solutions Architect and Full Stack Developer, I have strong skills in React.js and Node.js, which I believe align well with the projects at your company. I'm particularly interested in how FirstStore Ricoz and would love to know if there are any openings that fit my background.
+
+# Could we possibly connect for a quick chat?
