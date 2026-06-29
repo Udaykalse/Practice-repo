3c4f77c84ce29970a6466a80ebc829f301bbd411
Udaykalse <udaysinhk.2000@gmail.com>
2026-06-29 17:55:02 +0530

    Add examples for inheritance, polymorphism, encapsulation, abstraction, and interfaces D-3

---
+++ java/j3/isPrime.java
@@ -0,0 +1,7 @@
+public boolean isPalindrome(String str) {
+    int i = 0, j = str.length() - 1;
+    while (i < j) {
+        if (str.charAt(i++) != str.charAt(j--)) return false;
+    }
+    return true;
+}
