1fd8fd6d665ca68b3c38479a1971cc2ee73df1f3
Udaykalse <udaysinhk.2000@gmail.com>
2026-07-11 23:19:56 +0530

    Initial commit: JavaScript interview practice questions #Day-80

---
+++ JS/j80/app4.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3];
+
+let result = arr.map(x => x * 2)
+                .filter(x => x > 3);
+
+console.log(result);
---
+++ JS/j80/app5.js
@@ -0,0 +1,12 @@
+let a = 10;
+
+function demo() {
+    console.log(a);
+}
+
+function run() {
+    let a = 20;
+    demo();
+}
+
+run();
