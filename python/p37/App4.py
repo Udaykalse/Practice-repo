f0c3f8228db017702fcfdf4a1cf9c2e822a4be2b
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-03 11:37:42 +0530

    Demonstrate variable usage, data types, and basic arithmetic in Python

---
+++ python/p37/App4.py
@@ -0,0 +1,11 @@
+name = "Amey"
+age = 25
+city = "Latur"
+lang = 'Python'
+learning=True
+print("Name:",name)
+print("Age:",age)
+print("Favorite programming language:",lang)
+print("Currently learning Python:",learning)
+
+print("Next year my age will be:",age+1)
