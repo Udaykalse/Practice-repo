96683d66f5f06a4414ed77e7e62c556290e98826
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-03 11:36:24 +0530

    Add examples of string slicing, splitting, and replacement operations

---
+++ python/p37/App3.py
@@ -0,0 +1,13 @@
+text = "Python is easy to learn"
+
+print(text[0:6])
+print(text.split()[0])
+print(text.split()[2])
+print(text.replace("easy","powerful"))
+print(text.split())
+print(len(text.split()))
+print("""I am learning Python.
+Python is powerful.""")
+
+
+
