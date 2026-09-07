6c1b6bfdc76c9f5962283e26285553400cf3c476
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-07 23:19:46 +0530

    Demonstrate various set operations and methods

---
+++ python/p38/demo25.py
@@ -0,0 +1,11 @@
+skills = {"Python","React","Javascript" "SQL", "Python", "Git", "SQL"}
+print(skills)
+skills.add("FastAPI")
+print(skills)
+skills.remove("Git")
+print(skills)
+skills.discard("Java")
+print(skills)
+print(skills)
+print(len(skills))
+
