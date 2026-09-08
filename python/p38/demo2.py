8b0aabe32acdf6f26330997ad609fb74488869d6
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-08 22:29:39 +0530

    Add Python script for variables and data types

---
+++ python/p38/demo2.py
@@ -0,0 +1,24 @@
+name = "Amey"
+age = 25
+city = "Latur"
+lang = 'Python'
+learning=True
+height=6.1
+num = "25"
+print("Name:",name)
+print("Age:",age)
+print("Favorite programming language:",lang)
+print("Currently learning Python:",learning)
+print("Next year my age will be:",age+1)
+print("After year my age will be:",age+5)
+
+print("My Height",height)
+
+
+print(type(name))
+print(type(age))
+print(type(lang))
+print(type(learning))
+print(type(age+1))
+print(type(height))
+print(type(num)) # string
