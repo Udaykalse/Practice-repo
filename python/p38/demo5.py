0a5dd0edbb9e64b53cacf7922cbf5cc5411c15bb
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-09 23:09:59 +0530

    Add Python arithmetic, comparison, and logical operators examples

---
+++ python/p38/demo5.py
@@ -0,0 +1,23 @@
+a = 17
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+print(a > b) # True
+print(a < b)   # False
+print(a == b)   # False
+print(a != b)   # True
+print(a >= 17)   # True
+print(b <= 5)   # True
+
+
+print(a > 10 and b < 10)   #True
+print(a > 20 or b < 10)   #True
+print(not(a > b))   #False
